f3673f909bbd913799f962fd1ab4530fe7586831
Allen B <pingguo.ny@gmail.com>
2018-08-30 20:46:49 -0400

    stuff

--- game.js
+++ game.js
@@ -1,19 +1,42 @@
-class Game {
+class GameData {
 	constructor() {
 		this.map = []
 		this.cities = []
 		this.generals = []
 	}
+	/** Feeds data.generals to the game */
 	feed_generals_data(generals) {
-		
+		this.generals = generals.map((general, playerIndex) => {
+			// if general in new generals array, return that
+			if(general !== -1)
+				return general;
+			// otherwise return this.generals[index], which might contain 
+			else
+				return this.generals[playerIndex];
+		});
 	}
+	/** Feeds data.map_diff to the game */
 	feed_map_data(map_diff) {
 		this.map = Game.patch(this.map, map_diff)
 	}
+	/** Feeds data.cities_diff to the game */
 	feed_cities_data(cities_diff) {
 		this.cities = Game.patch(this.cities, cities_diff)
 	}
-	
+	/** Convenience function. Calls
+
+	> this.feed_generals_data(data.generals)
+	> this.feed_map_data(data.map_diff)
+	> this.feed_cities_data(data.cities_diff)
+
+	*/
+	feed_data(data) {
+		this.feed_generals_data(data.generals);
+		this.feed_map_data(data.map_diff);
+		this.feed_cities_data(data.cities_diff);
+	}
+
+	/** Patching function. Taken from http://dev.generals.io/api#tutorial */
 	static patch(old, diff) {
 		var out = [];
 		var i = 0;
@@ -31,3 +54,5 @@ class Game {
 		return out;
 	}
 }
+
+module.exports = GameData;
--- main.js
+++ main.js
@@ -1,9 +1,61 @@
+#!/usr/bin/env node
+const GameData = require("./game.js");
+const io = require('socket.io-client');
+const socket = io('http://botws.generals.io');
 
-class Player {
+var gameData = new GameData();
+socket.on("disconnect", function() {
+	console.error("Disconnected");
+	process.exit(1);
+});
+socket.on("connect", function() {
+	console.log("Connected");
+	var user_id = process.env.BOT_USER_ID;
+	socket.emit("join_private", "test", user_id);
+	socket.emit("set_force_start", "test", true);
+	console.log("Joined http://bot.generals.io/games/test");
+	
+});
+var playerIndex = -1;
+socket.on("game_start", function(data) {
+	playerIndex = data.playerIndex;
+	var replay_url = 'http://bot.generals.io/replays/' + encodeURIComponent(data.replay_id);
+	console.log('Game starting! Replay: ' + replay_url)
+});
 
-}
-class Bot extends Player {
+socket.on("game_update", function(data) {
+	gameData.feed_data(data);
 
-}
+	while (true) {
+		// Pick a random tile.
+		var index = Math.floor(Math.random() * size);
 
+		// If we own this tile, make a random move starting from it.
+		if (terrain[index] === playerIndex) {
+			var row = Math.floor(index / width);
+			var col = index % width;
+			var endIndex = index;
 
+			var rand = Math.random();
+			if (rand < 0.25 && col > 0) { // left
+				endIndex--;
+			} else if (rand < 0.5 && col < width - 1) { // right
+				endIndex++;
+			} else if (rand < 0.75 && row < height - 1) { // down
+				endIndex += width;
+			} else if (row > 0) { //up
+				endIndex -= width;
+			} else {
+				continue;
+			}
+
+			// Would we be attacking a city? Don't attack cities.
+			if (cities.indexOf(endIndex) >= 0) {
+				continue;
+			}
+
+			socket.emit('attack', index, endIndex);
+			break;
+		}
+	}
+});
