6f951d7c6737e8b8ac5909caf571be4a668ad784
Allen B <pingguo.ny@gmail.com>
2018-08-31 09:42:23 -0400

    stuff

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function join_game(user_id) {
 	socket.emit("join_private", game_id, user_id);
 
 	// TODO: Add commands instead of this
+	// TODO: When a player says hi 1st time return "hi", 2nd time "hello", 3rd time nothing
 	socket.emit("set_force_start", game_id, true);
 	console.log("Joined http://bot.generals.io/games/" + encodeURI(game_id));
 }
@@ -45,13 +46,41 @@ socket.on("game_update", function(data) {
 		return arr;
 	}, []);
 
+	// Find all tiles that have an enemy next to them
+	var tiles_w_enemy = tiles.filter((tile_index) => {
+		for(adj_tile of [tile_index + 1, tile_index - 1, tile_index + game.width, tile_index - game.width]) {
+			if(game.terrain[adj_tile] >= 0 && game.terrain[adj_tile] !== playerIndex) {
+				return true;
+			}
+		}
+	});
+
+	while(true) {
+		// If there are no tiles left break
+		if(tiles_w_enemy.length === 0) break;
+
+		// Pick a random tile.
+		var index = Math.floor(Math.random() * tiles_w_enemy.length);
+		var tile_index = tiles_w_enemy[index];
+		// Remove tile from possible armies next time (so that the same tile isn't chosen every single time)
+		tiles_w_enemy.splice(index, 1);
+
+		// For each adjacent tile, attack
+		for(adj_tile of [tile_index + 1, tile_index - 1, tile_index + game.width, tile_index - game.width]) {
+			if(game.terrain[adj_tile] >= 0 && game.terrain[adj_tile] !== playerIndex && game.armies[adj_tile] < game.armies[tile_index]) {
+				console.log("Attacking opponent!")
+				socket.emit("attack", tile_index, adj_tile);
+				return;
+			}
+		}
+	}
+
 	// Tiles with an adjacent empty tile
 	var tiles_pool = tiles.slice();
 	// Find a tile with an adjacent empty tile
 	while (true) {
-		// If there are no more tiles to choose from then finish turn
-		if(tiles_pool.length === 0) 
-			break;
+		// If there are no more tiles to choose from then break
+		if(tiles_pool.length === 0) break;
 
 		// Pick a random tile.
 		var index = Math.floor(Math.random() * tiles_pool.length);
@@ -59,20 +88,12 @@ socket.on("game_update", function(data) {
 		// Remove tile from possible armies next time (so that the same tile isn't chosen every single time)
 		tiles_pool.splice(index, 1);
 
-		// TODO: Check for cities
-		// If it has an empty tile next to it, attack
-		if(game.terrain[tile_index + 1] === gamelib.Tile.EMPTY) {
-			socket.emit("attack", tile_index, tile_index + 1);
-			return;
-		} else if(game.terrain[tile_index - 1] === gamelib.Tile.EMPTY) {
-			socket.emit("attack", tile_index, tile_index - 1);
-			return;
-		} else if(game.terrain[tile_index + game.width] === gamelib.Tile.EMPTY) {
-			socket.emit("attack", tile_index, tile_index + game.width);
-			return;
-		} else if(game.terrain[tile_index - game.width] === gamelib.Tile.EMPTY) {
-			socket.emit("attack", tile_index, tile_index - game.width);
-			return;
+		// If it has an empty tile next to it that has a nonpositive army (empty squares and cities/neutral squares with negative army), attack
+		for(adj_tile of [tile_index + 1, tile_index - 1, tile_index + game.width, tile_index - game.width]) {
+			if(game.terrain[adj_tile] === gamelib.Tile.EMPTY && (game.cities.indexOf(adj_tile) < 0 || game.armies[adj_tile] < 0)) {
+				socket.emit("attack", tile_index, adj_tile);
+				return;
+			}
 		}
 	}
 
